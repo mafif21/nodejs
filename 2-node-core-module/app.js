1707e1bdf5bfbf7aae052e69a53620d2e23a020b
unknown <afif.maliki21@gmail.com>
2023-01-23 02:19:15 +0700

    making directory

--- 2-node-core-module/app.js
+++ 2-node-core-module/app.js
@@ -3,8 +3,11 @@
 // file system
 const fs = require("node:fs");
 
+//making directory
+fs.mkdirSync("data");
+
 //writing string to file from js ( synchronus )
 fs.writeFileSync(
-  "test.txt",
+  "data/test.txt",
   "Hi file ini ditulis secara sinkronus menggunakan javascript"
 );
