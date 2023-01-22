907735f1dded1485813ecaba68a08863a3399325
unknown <afif.maliki21@gmail.com>
2023-01-23 02:22:34 +0700

    adding try catch

--- 2-node-core-module/app.js
+++ 2-node-core-module/app.js
@@ -3,11 +3,16 @@
 // file system
 const fs = require("node:fs");
 
-//making directory
-fs.mkdirSync("data");
+// using try catch because asynchronus
+try {
+  //making directory
+  fs.mkdirSync("data");
 
-//writing string to file from js ( synchronus )
-fs.writeFileSync(
-  "data/test.txt",
-  "Hi file ini ditulis secara sinkronus menggunakan javascript"
-);
+  //writing string to file from js ( synchronus )
+  fs.writeFileSync(
+    "data/test.txt",
+    "Hi file ini ditulis secara sinkronus menggunakan javascript"
+  );
+} catch (error) {
+  console.log(error);
+}
