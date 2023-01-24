ad4d4d8c6f2072399f60c458c08114e703dd4aaa
unknown <afif.maliki21@gmail.com>
2023-01-25 05:25:17 +0700

    how to use npm

--- 3-npm/index.js
+++ 3-npm/index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+import validator from "validator";
+import chalk from "chalk";
+
+//chalk
+console.log(chalk.blue("Hello world!"));
+// const error = "Error";
+// console.log(chalk.black.bgRed(error));
+
+//validator
+console.log(validator.isEmail("afif@gmail.com"));
+// console.log(validator.isMobilePhone("1393764225", "id-ID"));
+// console.log(validator.isNumeric("1393764225a"));
