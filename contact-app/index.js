90ca374ad122e3987e1cb19cd503262a1247f968
unknown <afif.maliki21@gmail.com>
2023-01-25 18:16:44 +0700

    make custom command line

--- contact-app/index.js
+++ contact-app/index.js
@@ -1,14 +1,40 @@
-const contacts = require("./contacts");
+const yargs = require("yargs");
+const { save } = require("./contacts");
 
-const main = async () => {
-  try {
-    const name = await contacts.userQuestion("Whats is your name? ");
-    const phone = await contacts.userQuestion("Whats your phone number? ");
+// mengambil argumen dari command line
+yargs.command({
+  command: "add",
+  describe: "Adding new data",
+  builder: {
+    name: {
+      describe: "User Name",
+      demandOption: true,
+      type: "string",
+    },
+    phone: {
+      describe: "Phone Number",
+      demandOption: true,
+      type: "int",
+    },
+  },
+  handler(argv) {
+    save(argv.name, argv.phone);
+  },
+});
 
-    contacts.save(name, phone);
-  } catch (error) {
-    console.log(error);
-  }
-};
+yargs.parse();
 
-main();
+// const contacts = require("./contacts");
+
+// const main = async () => {
+//   try {
+//     const name = await contacts.userQuestion("Whats is your name? ");
+//     const phone = await contacts.userQuestion("Whats your phone number? ");
+
+//     contacts.save(name, phone);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// main();
