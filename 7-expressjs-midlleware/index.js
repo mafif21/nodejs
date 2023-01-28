7b16c4f1f9c96e326c13a2bf68b948bbf5421f66
unknown <afif.maliki21@gmail.com>
2023-01-28 18:23:34 +0700

    app level middleware

---
+++ 7-expressjs-midlleware/index.js
@@ -0,0 +1,74 @@
+import express from "express";
+import expressLayouts from "express-ejs-layouts";
+
+const app = express();
+const port = 3000;
+
+// data
+const students = [
+  {
+    name: "Muhammad Afif",
+    nim: 1202202052,
+    active: true,
+  },
+  {
+    name: "Ahmad Sutanji",
+    nim: 388371733919,
+    active: true,
+  },
+  {
+    name: "Hasan Mustofa",
+    nim: 12028383746,
+    active: false,
+  },
+];
+
+// setting template engine to ejs
+app.set("view engine", "ejs");
+
+// confugure using ejs layouts
+app.use(expressLayouts);
+
+// middleware
+// harus ada next kalau tidak maka akan hang
+app.use((req, res, next) => {
+  console.log("Time:", Date.now() * 7);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("ini middleware ke dua");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    name: "lele",
+    title: "Home",
+    students,
+    layout: "layouts/main",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About", layout: "layouts/main" });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { layout: "layouts/main" });
+});
+
+app.get("/product/:id", (req, res) => {
+  res.send(
+    `Product id : ${req.params.id} <br /> Category : ${req.query.category}`
+  );
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
+
+app.listen(port, () => {
+  console.log("Connect!!!");
+});
