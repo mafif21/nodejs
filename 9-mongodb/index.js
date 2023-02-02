1de972185791c3e2ea563641c3e5a638c7bf76f0
unknown <afif.maliki21@gmail.com>
2023-02-02 21:09:13 +0700

    insert one data

--- 9-mongodb/index.js
+++ 9-mongodb/index.js
@@ -10,8 +10,17 @@ client.connect((err, client) => {
   if (err) return console.log("cant connect this local db");
 
   // collection name
-  const colName = "coba";
+  const dbName = "coba";
 
   // choose collection
-  const db = client.db(colName);
+  const db = client.db(dbName);
+
+  // insert one data to mongodb
+  db.collection("user").insertOne(
+    { name: "Paijo", age: 80, isAdmin: true },
+    (err, res) => {
+      if (err) return console.log("Data cant input in database");
+      console.log("Data success input");
+    }
+  );
 });
