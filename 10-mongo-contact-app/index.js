1391be08c15c08a07dc20af62d4728116b33d523
unknown <afif.maliki21@gmail.com>
2023-02-03 23:49:07 +0700

    make delete data from database

--- 10-mongo-contact-app/index.js
+++ 10-mongo-contact-app/index.js
@@ -3,11 +3,16 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const expressEjsLayouts = require("express-ejs-layouts");
 const user = require("./model/User");
+const methodOverride = require("method-override");
 
 // flash message
 const session = require("express-session");
 const flash = require("connect-flash");
 const cookie = require("cookie-parser");
+
+// validator
+const { body, validationResult, check } = require("express-validator");
+const { createIndexes } = require("./model/User");
 
 const app = express();
 const port = 3000;
@@ -28,6 +33,9 @@ app.use(
   })
 );
 app.use(flash());
+
+// configure override method
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home", { title: "Homepage", layout: "layouts/main" });
@@ -51,6 +59,57 @@ app.get("/contact", async (req, res) => {
   }
 });
 
+app.post(
+  "/contact",
+  [
+    body("name").custom(async (value) => {
+      const duplicate = await user.findOne({ name: value });
+      if (duplicate) {
+        throw new Error("Name already exists");
+      }
+      return true;
+    }),
+    check("email", "Domain not valid").isEmail(),
+    check("phone", "Must indonesian number").isMobilePhone("id-ID"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("add", {
+        layout: "layouts/main",
+        title: "Add",
+        errors: errors.array(),
+      });
+    } else {
+      //cara 1
+      // const newUser = new user({
+      //   name: req.body.name,
+      //   phone: req.body.phone,
+      //   email: req.body.email,
+      // });
+      // newUser.save((error, result) => {
+      //   if (error) return console.log("add data failed");
+      //   req.flash("msg", "Add data success");
+      //   res.redirect("/contact");
+      // });
+
+      // cara 2
+      user.insertMany(req.body, (error, result) => {
+        if (error) return console.log("add data failed");
+        req.flash("msg", "Add data success");
+        res.redirect("/contact");
+      });
+    }
+  }
+);
+
+app.get("/contact/add", (req, res) => {
+  res.render("add", {
+    title: "Add",
+    layout: "layouts/main",
+  });
+});
+
 app.get("/contact/:name", async (req, res) => {
   const contact = await user.findOne({ name: req.params.name });
   try {
@@ -62,6 +121,31 @@ app.get("/contact/:name", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+});
+
+// delete option 1
+// app.get("/contact/delete/:name", async (req, res) => {
+//   const findData = await user.findOne({ name: req.params.name });
+//   if (!findData) {
+//     res.status(404);
+//     res.send("<h1>Data not found</h1>");
+//   } else {
+//     await user.deleteOne({ name: req.params.name });
+//     req.flash("msg", "Delete success");
+//     res.redirect("/contact");
+//   }
+// });
+
+// delete option 2
+app.delete("/contact", async (req, res) => {
+  await user.deleteOne({ name: req.body.name });
+  req.flash("msg", "Delete success");
+  res.redirect("/contact");
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.send("<h1>Not Found</h1>");
 });
 
 app.listen(port, () => console.log("connected"));
